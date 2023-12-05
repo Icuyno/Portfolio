dec18368b087003bccd12971a49d44027a1dd14f
Icuyno <isabellecuyno@yahoo.com>
2023-12-05 07:08:09 -0800

    fixed letteboxd live link

--- partials/left-aside.php
+++ partials/left-aside.php
@@ -1,4 +1,3 @@
-
 <?php
 $currentPage = basename($_SERVER['PHP_SELF']);
 $projectPages = array(
@@ -9,7 +8,7 @@ $projectPages = array(
         'backButtonIcon' => './assets/icons/home.svg',
         'backButtonText' => 'Go Back Home',
         'nextButtonText' => 'Next Project',
-        'nextButtonIcon' => './assets/icons/next-600.svg',// Replace with the actual icon path
+        'nextButtonIcon' => './assets/icons/next-600.svg', // Replace with the actual icon path
     ),
     'devote.php' => array(
         'liveLink' => 'http://devote-tarot.isabellecuyno.com/',
@@ -30,7 +29,7 @@ $projectPages = array(
         'nextButtonIcon' => './assets/icons/next-600.svg',
     ),
     'letterboxd.php' => array(
-        'liveLink' => 'https://glazemedia.princessrabano.ca/blog-post/',
+        'liveLink' => 'https://www.figma.com/file/Ut3aokpiuAfgvvW4ZXWkmq/Letterbox-Wrapped?type=design&node-id=0%3A1&mode=design&t=wXmEsPQV7FmJFKZt-1',
         'nextLink' => 'index.php', // Replace with the actual link for the next project
         'backLink' => 'coastalrolls.php',
         'backButtonIcon' => './assets/icons/back-600.svg',
@@ -117,27 +116,27 @@ $isWorkPage = ($currentPage === 'work.php'); // Check if the current page is wor
             </div>
             <?php
             if ($isWorkPage) {
-                    echo '<div class="filter-container">';
-                    echo '<div class="text-container">';
-                    echo 'Filter by project';
-                    echo '</div>';
-                    echo '<button class="filter-btn" onclick="filterProjects(\'All\')">';
-                    echo 'All';
-                    echo '</button>';
-                    echo '<button class="filter-btn" onclick="filterProjects(\'UI/UX\')">';
-                    echo 'UI/UX';
-                    echo '</button>';
-                    echo '<button class="filter-btn" onclick="filterProjects(\'Web Development\')">';
-                    echo 'Web Development';
-                    echo '</button>';
-                    echo '<button class="filter-btn" onclick="filterProjects(\'Product Design\')">';
-                    echo 'Product Design';
-                    echo '</button>';
-                    echo '<button class="filter-btn" onclick="filterProjects(\'Collaborative\')">';
-                    echo 'Collaborative';
-                    echo '</button>';
-                    echo '</div>';
-                }
+                echo '<div class="filter-container">';
+                echo '<div class="text-container">';
+                echo 'Filter by project';
+                echo '</div>';
+                echo '<button class="filter-btn" onclick="filterProjects(\'All\')">';
+                echo 'All';
+                echo '</button>';
+                echo '<button class="filter-btn" onclick="filterProjects(\'UI/UX\')">';
+                echo 'UI/UX';
+                echo '</button>';
+                echo '<button class="filter-btn" onclick="filterProjects(\'Web Development\')">';
+                echo 'Web Development';
+                echo '</button>';
+                echo '<button class="filter-btn" onclick="filterProjects(\'Product Design\')">';
+                echo 'Product Design';
+                echo '</button>';
+                echo '<button class="filter-btn" onclick="filterProjects(\'Collaborative\')">';
+                echo 'Collaborative';
+                echo '</button>';
+                echo '</div>';
+            }
             ?>
             <footer class="linksFootermobile">
                 <div class="MFooterGridRow">
@@ -150,9 +149,12 @@ $isWorkPage = ($currentPage === 'work.php'); // Check if the current page is wor
                     <div class="MFooterName">Network(s)</div>
                     <div class="MFooterLink">
                         <a target="_blank" rel="noopener" href="https://www.linkedin.com/in/izzy-cuyno-design/">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="23" viewBox="0 0 20 23" fill="none">
-                            <path d="M18.5714 1.42871H1.42411C0.638393 1.42871 0 2.07603 0 2.87068V19.9867C0 20.7814 0.638393 21.4287 1.42411 21.4287H18.5714C19.3571 21.4287 20 20.7814 20 19.9867V2.87068C20 2.07603 19.3571 1.42871 18.5714 1.42871ZM6.04464 18.5716H3.08036V9.02693H6.04911V18.5716H6.04464ZM4.5625 7.72335C3.61161 7.72335 2.84375 6.95103 2.84375 6.0046C2.84375 5.05818 3.61161 4.28585 4.5625 4.28585C5.50893 4.28585 6.28125 5.05818 6.28125 6.0046C6.28125 6.9555 5.51339 7.72335 4.5625 7.72335ZM17.1562 18.5716H14.192V13.9287C14.192 12.8216 14.1696 11.3975 12.6518 11.3975C11.1071 11.3975 10.8705 12.6028 10.8705 13.8484V18.5716H7.90625V9.02693H10.75V10.3305H10.7902C11.1875 9.5805 12.1563 8.79032 13.5982 8.79032C16.5982 8.79032 17.1562 10.768 17.1562 13.3394V18.5716Z" fill="black"/>
-                        </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="23" viewBox="0 0 20 23"
+                                fill="none">
+                                <path
+                                    d="M18.5714 1.42871H1.42411C0.638393 1.42871 0 2.07603 0 2.87068V19.9867C0 20.7814 0.638393 21.4287 1.42411 21.4287H18.5714C19.3571 21.4287 20 20.7814 20 19.9867V2.87068C20 2.07603 19.3571 1.42871 18.5714 1.42871ZM6.04464 18.5716H3.08036V9.02693H6.04911V18.5716H6.04464ZM4.5625 7.72335C3.61161 7.72335 2.84375 6.95103 2.84375 6.0046C2.84375 5.05818 3.61161 4.28585 4.5625 4.28585C5.50893 4.28585 6.28125 5.05818 6.28125 6.0046C6.28125 6.9555 5.51339 7.72335 4.5625 7.72335ZM17.1562 18.5716H14.192V13.9287C14.192 12.8216 14.1696 11.3975 12.6518 11.3975C11.1071 11.3975 10.8705 12.6028 10.8705 13.8484V18.5716H7.90625V9.02693H10.75V10.3305H10.7902C11.1875 9.5805 12.1563 8.79032 13.5982 8.79032C16.5982 8.79032 17.1562 10.768 17.1562 13.3394V18.5716Z"
+                                    fill="black" />
+                            </svg>
                         </a>
                     </div>
                 </div>
@@ -160,4 +162,3 @@ $isWorkPage = ($currentPage === 'work.php'); // Check if the current page is wor
         </div>
     </div>
 </div>
-
