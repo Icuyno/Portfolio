c07f62a319e155ee6d227f4c749da5aecc19dfbe
Icuyno <isabellecuyno@yahoo.com>
2023-12-13 19:53:31 -0800

    - github link is added - image on Food Query is updated to the correct dimensions

--- partials/head.php
+++ partials/head.php
@@ -1,17 +1,26 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Izzy Cuyno, a web designer with a pioneering spirit and I intend to create memorable interfaces and experiences that delight the senses.">
+    <meta name="author" content="Izzy Cuyno">
+    <meta name="robots" content="index, follow">
+    <meta property="og:title" content="Izzy Cuyno - Web Designer">
+    <meta property="og:description" content="Izzy Cuyno, a web designer with a pioneering spirit and I intend to create memorable interfaces and experiences that delight the senses.">
+    <meta property="og:image" content="https://images.pickapic.live/get/d3bf6aaf-e094-2396-2bd8-ae0b85b4a1d8-1702440599.png">
+    <meta property="og:url" content="https://isabellecuyno.com/">
     <title>Izzy Cuyno - Web Designer</title>
     <link rel="icon" type="image/png" href="img/favicon.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter+Tight:wght@400;600;700&display=swap" rel="stylesheet"> 
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link href="https://fonts.googleapis.com/css2?family=Inter+Tight:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
     <link rel="stylesheet" href="./css/reset.css">
     <link rel="stylesheet" href="./css/common.css">
     <link rel="stylesheet" href="./css/main.css">
 </head>
+
 <body>
